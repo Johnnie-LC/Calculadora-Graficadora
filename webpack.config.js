2bcc401af5bf047c127eb813ff11f6158b947a9b
Johnnie Lopez <johesloc@gmail.com>
2022-01-22 17:53:51 -0500

    feat: scritp webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/main.js',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
